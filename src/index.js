7188b84694dfff58e24edd4177c5403f3bbe25bd
M.S.T.O.P <mstop4@gmail.com>
2019-04-10 11:10:49 -0400

    Pixi smoke test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { listThings } from './js/helloworld';
+import * as PIXI from 'pixi.js';
+
+let type = 'WebGL';
+if (!PIXI.utils.isWebGLSupported()) {
+  type = 'canvas';
+}
+
+PIXI.utils.sayHello(type);
 
 listThings([1, 2, 3, 4, 5]);
